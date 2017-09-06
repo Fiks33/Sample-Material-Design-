e79506225ae3fd446e346e0476d7e9200f9d3225
Taufik <thidayat13@gmail.com>
2017-09-06 13:59:22 +0700

    first commit

---
+++ app/src/main/java/com/trust/android/samplematerialdesign/fragment/TabFragment.java
@@ -0,0 +1,85 @@
+package com.trust.android.samplematerialdesign.fragment;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.trust.android.samplematerialdesign.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Fik on 15-Jul-17.
+ */
+
+public class TabFragment extends Fragment {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    ViewPagerAdapter pagerAdapter;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_tab, container, false);
+
+        viewPager = (ViewPager)view. findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout)view. findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+        return view;
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getChildFragmentManager());
+        adapter.addFragment(new OneFragment(), "ONE");
+        adapter.addFragment(new TwoFragment(), "TWO");
+        adapter.addFragment(new ThreeFragment(), "THREE");
+        viewPager.setAdapter(adapter);
+    }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+
+}
---
+++ app/src/main/java/com/trust/android/samplematerialdesign/fragment/TwoFragment.java
@@ -0,0 +1,31 @@
+package com.trust.android.samplematerialdesign.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.trust.android.samplematerialdesign.R;
+
+/**
+ * Created by Fik on 13-Jul-17.
+ */
+
+public class TwoFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_two, container, false);
+
+
+        return view;
+    }
+}
